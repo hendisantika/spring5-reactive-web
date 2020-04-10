9d429600dfad34fb06fe5a5701a5fd878f23d0e0
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-10 07:23:26 +0700

    #6 - Add Product modelgetProductFromRepository in ProductHandler

---
+++ src/main/java/com/hendisantika/spring5reactiveweb/handler/ProductHandler.java
@@ -0,0 +1,18 @@
+package com.hendisantika.spring5reactiveweb.handler;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring5-reactive-web
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/20
+ * Time: 07.23
+ */
+public interface ProductHandler {
+    Mono<ServerResponse> getProductFromRepository(ServerRequest request);
+}
