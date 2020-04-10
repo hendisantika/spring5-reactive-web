5871e4d22317e4b1093daa016de7a2cbcf55506b
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-10 07:17:40 +0700

    #1 - iInitial commit

---
+++ src/main/java/com/hendisantika/spring5reactiveweb/Server.java
@@ -0,0 +1,14 @@
+package com.hendisantika.spring5reactiveweb;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring5-reactive-web
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/20
+ * Time: 07.16
+ */
+public class Server {
+
+}
