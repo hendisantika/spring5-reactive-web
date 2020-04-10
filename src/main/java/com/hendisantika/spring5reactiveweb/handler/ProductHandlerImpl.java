70183eb464e2394fc6ff9be77667543b9fb382d6
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-10 07:25:07 +0700

    #9 - Add ProductHandlerImpl

---
+++ src/main/java/com/hendisantika/spring5reactiveweb/handler/ProductHandlerImpl.java
@@ -0,0 +1,20 @@
+package com.hendisantika.spring5reactiveweb.handler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring5-reactive-web
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/20
+ * Time: 07.24
+ */
+public class ProductHandlerImpl implements ProductHandler {
+
+    private final ProductRepository repository;
+
+    public ProductHandlerImpl(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+}
